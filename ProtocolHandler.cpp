9568a4fd846a1187274b21c723b7a29e9109a319
Gydo194 <gydo194@omen>
2018-03-18 21:34:49 +0100

    for now; http param system is working, maybe can be improved.

--- ProtocolHandler.cpp
+++ ProtocolHandler.cpp
@@ -6,20 +6,71 @@ using namespace std;
 
 ProtocolHandler::ProtocolHandler()
 {
+    keyBuffer = string();
+    valueBuffer = string();
 }
 
 ProtocolHandler::~ProtocolHandler()
 {
 }
 
-void ProtocolHandler::processInput(char *input) {
+void ProtocolHandler::processInput(char *input)
+{
     cout << "[PROTOCOLHANDLER] processInput called\n";
+
+
+
+
+    
+    //print every character of string
     for(size_t i = 0; i < strlen(input); i++) {
-        printf("CHAR: '%c'\n",*(input+i));
-        
+        //printf("CHAR: '%c'\n",*(input+i));
+        processChar(*(input+i));
+    }
+    append();
+    
+}
+
+void ProtocolHandler::processChar(char in)
+{
+   //cout << "running char handler\n";
+    switch(in) {
+    case '&':
+        state = KEY; //reset to reading into KEY buffer
+        append();
+        break;
+    case '=':
+        state = VALUE; //read into VALUE buffer from now
+        break;
+    default:
+        processNewChar(in); //read input char into correct buffer
+        break;
     }
 }
 
-void ProtocolHandler::processChar(char in) {
-    
+void ProtocolHandler::processNewChar(char in)
+{
+    switch(state) {
+    case KEY:
+        cout << "appending '" << in << "' to KEY buffer\n";
+        //keyBuffer.append(in,1);
+        keyBuffer += in;
+        break;
+    case VALUE:
+        cout << "appending '" << in << "' to VALUE buffer\n";
+        //valueBuffer.append(in,1);
+        valueBuffer += in;
+        break;
+
+    }
+}
+
+void ProtocolHandler::append()
+{
+    cout << "STORING '" << keyBuffer << "' w/val '" << valueBuffer << "'.\n";
+    //store data
+    values.insert(std::pair<string,string>(keyBuffer,valueBuffer));
+    //clear buffers
+    keyBuffer.clear();
+    valueBuffer.clear();
 }
--- ProtocolHandler.h
+++ ProtocolHandler.h
@@ -6,6 +6,12 @@
 #define PH_PAIR_DELIMITER '&'
 #define PH_VALUE_DELIMITER '='
 
+#include <cstring>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
 class ProtocolHandler
 {
 public:
@@ -14,6 +20,12 @@ public:
     void processInput(char *input);
 private:
     void processChar(char in);
+    void processNewChar(char in);
+    void append();
+    string keyBuffer;
+    string valueBuffer;
+    map<string,string> values;
+    enum states { KEY,VALUE } state;
 };
 
 #endif // PROTOCOLHANDLER_H
